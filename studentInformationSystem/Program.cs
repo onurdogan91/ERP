1b4c85b4eb8bf60b06325b730d02b515a6815f85
onurdogan91 <onurdogan@outlook.com.tr>
2022-09-19 20:35:55 +0300

    deneme

---
+++ studentInformationSystem/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace studentInformationSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Not Bilgisi Girmek İstediğiniz Öğrenci Ad ve Soyadını Giriniz: ");
+            String x = Console.ReadLine();
+            Console.WriteLine("Ders Adini Giriniz: ");
+            String y = Console.ReadLine();
+            Console.WriteLine("Ders Kodu Giriniz: ");
+            int z = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Vize Notu Giriniz: ");
+            int q = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Vize Notu Giriniz: ");
+            int w = Convert.ToInt32(Console.ReadLine());
+
+
+
+            Student s1234 = new Student();
+            s1234.NameSurname = x;
+            s1234.LessonName = y;
+            s1234.LessonCode = z;
+            s1234.ScoreVize = q;
+            s1234.ScoreFinal = w;
+
+            Console.WriteLine(s1234.GetProperties());
+            Console.WriteLine("Gecme Durumu : " + s1234.Calculations());
+            
+
+        }
+    }
+}
---
+++ studentInformationSystem/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studentInformationSystem
+{
+    public class Student
+    {
+        public string NameSurname;
+        public string LessonName;
+        public int LessonCode;
+        public int ScoreVize;
+        public int ScoreFinal;
+        public string Sonuc;
+
+        public string GetProperties()
+        {            
+            string S1234Properties = String.Format("Ad Soyad:{0} Ders Adi:{1} Ders Kodu:{2} Sınav Notu Vize:{3} Sınav Notu Final:{4} ", NameSurname, LessonName, LessonCode, ScoreVize, ScoreFinal ); 
+            return S1234Properties;
+        }
+
+        public string Calculations()
+        {
+            int Score = ((ScoreVize / 10) * 3) + ((ScoreFinal / 10) * 7); 
+
+            if (Score >= 50)
+            {
+                Sonuc = "Gecti" + " " + Score;
+            }
+            else 
+            {
+                Sonuc = "Kaldi" + " " + Score;
+            }
+            return Sonuc;
+        }
+
+
+
+    }
+}
