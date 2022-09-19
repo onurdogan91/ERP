b6bec3c784bb3bc75301e33c957114f2fd5eb78f
onurdogan91 <onurdogan@outlook.com.tr>
2022-09-19 19:36:28 +0300

    yeni

--- Encapsulation/Rectangle.cs
+++ Encapsulation/Rectangle.cs
@@ -8,12 +8,43 @@ namespace Encapsulation
 {
     public class Rectangle
     {
-        public int a;
-        public int b;
-
+        //public int a;
+        //public int b;
+        private int _a;
+        private int _b;
+        public int a
+        {
+            get { return _a; }
+            set
+            {
+                if (value < 0)
+                {
+                    _a = 0;
+                }
+                else
+                {
+                    _a = value;
+                }
+            }
+        }
+        public int b
+        {
+            get { return _b; }
+            set
+            {
+                if (value < 0)
+                {
+                    _b = 0;
+                }
+                else
+                {
+                    _b = value;
+                }
+            }
+        }
         public int CalculateArea()
         {
-            return a * b;
+            return _a * _b;
         }
     }
 }
