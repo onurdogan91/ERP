c88872e3d0cad0293fe0c468d39e3004fb102fbe
onurdogan91 <onurdogan@outlook.com.tr>
2022-09-22 20:43:58 +0300

    Odev

---
+++ Project1/Program.cs
@@ -0,0 +1,74 @@
+﻿using FakeData;
+using System.Data;
+
+namespace Project1
+{
+    class Program
+    {
+        static List<Student> students; = new List<Student>();
+            ////start:
+            //Student student = new Student();
+            //FakeData.NameData.GetFirstName();
+            //Console.WriteLine("Name : " + FakeData.NameData.GetFirstName());
+            //Console.WriteLine("Surname : " + FakeData.NameData.GetSurname() );
+            //Console.WriteLine("Age : " + FakeData.NumberData.GetNumber(18, 28));
+            ////goto start;
+        static void Main()
+        {
+            Doldur();
+        }
+        private static void Menu()
+        {
+            Console.WriteLine("1 - All Datas");
+            Console.WriteLine("2 - View Details");
+            Console.WriteLine("3 - New Student");
+            Console.WriteLine("4 - Remove");
+            Console.WriteLine("5 - Update ");
+            Console.WriteLine("6 - Search");
+            Console.WriteLine("7 - Sorting by Name(A-Z");
+            Console.WriteLine("8 - Sorting by Name(Z-A");
+            int select = Convert.ToInt32(Console.ReadLine());
+            if (select == 1)
+            {
+                Print(students);
+            }
+            else if(select == 2)
+            {
+                Details();
+            }
+            else if (select == 3)
+            {
+                Entry();
+            }
+            else if(select == 4)
+            {
+                Remove();
+            }
+            else if (select == 5)
+            {
+                Update();
+            }
+            else if (select == 6)
+            {
+                Search();
+            }
+            else if(select == 7)
+            {
+                SortingAZ();
+            }
+            else if( select == 8)
+            {
+                SortingZA();
+            }
+            else
+            {
+                Console.WriteLine("Wrong Choice");
+            }
+            Menu();
+        }
+        private static void SortingZA()
+        {
+            var ylist = students.OrderByDescending(x => x.Name).ToList();
+        }
+    }
+}
---
+++ Project1/Student.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public char Sex { get; set; }        
+        public DateTime BYear { get; set; }
+        public string street { get; set; }
+        public int DoorNo { get; set; }
+        public string City { get; set; }
+        public string District { get; set; }
+        public int Salary { get; set; }
+
+        public string GetTitle()
+        {
+            if (Sex == 'M')
+            {
+                return "Mr " + Name + " " + Surname;
+            }
+            else
+            {
+                return " Mrs " + Name + " " + Surname;
+            }
+        }
+
+        public int GetAge()
+        {
+            DateTime today = DateTime.Now;
+            int age = today.Year - BYear.Year;
+            DateTime BDay = BYear.AddYears(age);
+            if (BDay < today)
+            {
+                age++;
+            }
+            return age;
+        }
+
+        public List<String> GetAdress()
+        {
+            List<String> adress = new List<String>();
+            adress.Add(street);
+            adress.Add(DoorNo.ToString());            
+            adress.Add(District + " / " + City);
+            return adress;
+        }
+
+        public String GetAdress2()
+        {
+            return $" Şehir {street}\n No {DoorNo}\n" + $" {District + City}";
+        }
+
+        public string[] GetAdress3()
+        {
+            string[] adress = {street, DoorNo.ToString(), District, City};
+            return adress;
+        }
+    }
+}
